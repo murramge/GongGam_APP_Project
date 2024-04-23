b637b302759d45d17f67378e22f9f5afff345141
ssh8560 <ssh8560@naver.com>
2024-04-23 17:22:55 +0900

    feature: kopisApi 테스트 추가, gitIgnore에 __mocks__/react-native-config.ts 추가
    __mocks__/react-native-config.ts에 있는 API_KEY 업로드 안되게 gitignore에 추가

---
+++ __tests__/kopisApi.test.ts
@@ -0,0 +1,20 @@
+import {getShowDetail, getShowList} from '@apis/kopis';
+
+test('공연 목록 조회', async () => {
+  const size = 10;
+  const data = await getShowList({
+    startDate: '20240417',
+    endDate: '20240423',
+    page: 1,
+    size,
+  });
+  expect(data?.length).toBe(size);
+}, 10000);
+
+test('공연 상세 조회', async () => {
+  const data = await getShowDetail({
+    showId: 'PF239796',
+  });
+  console.log(data);
+  expect(data?.genrenm).toBe('복합');
+});
