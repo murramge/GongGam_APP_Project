75af5cbea6da3872b01352e4e0c87969418073c0
kangeunhwa <dmdgi37@naver.com>
2024-04-25 18:06:16 +0900

    feature:common-input-button

---
+++ src/components/inputs/SignInput.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import CommonInput from '../../atoms/inputs/CommonInput';
+
+interface SignInputProps {}
+
+const SignInput = ({}: SignInputProps) => {
+  return (
+    <View style={styles.SignInput}>
+      <CommonInput></CommonInput>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  SignInput: {
+    borderWidth: 1,
+    borderColor: '#838383',
+    margin: 15,
+    borderRadius: 24,
+    paddingHorizontal: 16,
+    backgroundColor: 'white',
+  },
+});
+
+export default SignInput;
