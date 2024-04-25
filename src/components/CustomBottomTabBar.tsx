067db73597c1b1abebcd53bc2e87b0a1632f7751
ssh8560 <ssh8560@naver.com>
2024-04-25 17:48:37 +0900

    feat: 더미데이터 삭제, style 분리

--- src/components/CustomBottomTabBar.tsx
+++ src/components/CustomBottomTabBar.tsx
@@ -1,6 +1,6 @@
 import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
-import React, {useState} from 'react';
-import {TouchableOpacity, View, useWindowDimensions} from 'react-native';
+import React from 'react';
+import {Dimensions, StyleSheet, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import 'react-native-get-random-values';
 import {v4} from 'uuid';
@@ -8,8 +8,8 @@ import {v4} from 'uuid';
 interface CustomBottomTabBarProps extends BottomTabBarProps {}
 
 const CustomBottomTabBar = ({navigation, state}: CustomBottomTabBarProps) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const {width} = useWindowDimensions();
+  const {routeNames} = state;
+  const currentRouteName = routeNames[state.index];
 
   const renderTabButton = ({
     name,
@@ -29,15 +29,12 @@ const CustomBottomTabBar = ({navigation, state}: CustomBottomTabBarProps) => {
           left: 16,
           right: 16,
         }}
-        style={{
-          width: 52,
-          height: 52,
-          elevation: isFocused ? 4 : 0,
-          borderRadius: 100,
-          backgroundColor: '#FFF',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        style={[
+          styles.tabButton,
+          {
+            elevation: isFocused ? 4 : 0,
+          },
+        ]}
         onPress={onPress}>
         <Icon name={name} size={28} color={isFocused ? '#3544C4' : '#aaa'} />
       </TouchableOpacity>
@@ -46,33 +43,13 @@ const CustomBottomTabBar = ({navigation, state}: CustomBottomTabBarProps) => {
 
   return (
     <View>
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          backgroundColor: 'transparent',
-          width,
-          height: 100,
-          padding: 16,
-        }}>
-        <View
-          style={{
-            borderRadius: 100,
-            backgroundColor: '#fff',
-            elevation: 4,
-            shadowColor: 'grey',
-            width: '100%',
-            height: '100%',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            flexDirection: 'row',
-          }}>
-          {routeNames.map((routeName, index) =>
+      <View style={styles.tabBarContainer}>
+        <View style={styles.tabBar}>
+          {routeNames.map(routeName =>
             renderTabButton({
-              name: routeName,
-              isFocused: index === currentIndex,
-              onPress: () => setCurrentIndex(index),
+              name: routeInfo[routeName].iconName,
+              isFocused: currentRouteName === routeName,
+              onPress: () => navigation.navigate(routeName),
             }),
           )}
         </View>
@@ -81,6 +58,46 @@ const CustomBottomTabBar = ({navigation, state}: CustomBottomTabBarProps) => {
   );
 };
 
-const routeNames = ['film-outline', 'people', 'chatbox-sharp', 'person'];
+const routeInfo: {
+  [key: string]: {
+    iconName: string;
+  };
+} = {
+  Home: {
+    iconName: 'film-outline',
+  },
+};
+
+const {width} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  tabBarContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'transparent',
+    width,
+    height: 100,
+    padding: 16,
+  },
+  tabBar: {
+    borderRadius: 100,
+    backgroundColor: '#fff',
+    elevation: 4,
+    shadowColor: 'grey',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  tabButton: {
+    width: 52,
+    height: 52,
+    borderRadius: 100,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default CustomBottomTabBar;
