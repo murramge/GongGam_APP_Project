4759e507ebff944ed101fec099af9bcbbc325997
전진영 <espoiryoung@naver.com>
2024-04-26 13:44:00 +0900

    상단 카드바 초기구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import MainUpperCardBar from '../atoms/cards/MainUpperCardBar';
+
 const Home = () => {
   return (
     <View>
       <Text>home</Text>
+      <MainUpperCardBar />
     </View>
   );
 };
+
 const styles = StyleSheet.create({});
 
 export default Home;
