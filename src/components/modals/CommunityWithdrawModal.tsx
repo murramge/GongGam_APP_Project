a9392bbd651507bb07896dd37fce749ad1a9c6d5
전진영 <espoiryoung@naver.com>
2024-05-05 22:42:48 +0900

    모임상세페이지 탈퇴모달 연결

--- src/components/modals/CommunityWithdrawModal.tsx
+++ src/components/modals/CommunityWithdrawModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text, Modal, StyleSheet, Pressable} from 'react-native';
-
 import LottieView from 'lottie-react-native';
 import {colors} from '@styles/color';
 import CommonButton from '../../atoms/buttons/CommonButton';
@@ -12,17 +11,6 @@ const CommunityWithdrawModal = ({
   isWithdrawModalOpen: boolean;
   onPressWithdrawCancel: () => void;
 }) => {
-
-import LottieView from 'lottie-react-native';
-import {colors} from '@styles/color';
-import CommonButton from '../../atoms/buttons/CommonButton';
-import {atom, useAtom, useSetAtom} from 'jotai';
-export const modalVisibleAtom = atom(false); //초기화
-
-const CommunityWithdrawModal = () => {
-  const [modalVisible, setModalVisible] = useAtom(modalVisibleAtom);
-
-
   return (
     <View>
       <Modal
@@ -110,11 +98,5 @@ const styles = StyleSheet.create({
     paddingBottom: 24,
   },
 });
-
-        visible={modalVisible}></Modal>
-    </View>
-  );
-};
-
 
 export default CommunityWithdrawModal;
