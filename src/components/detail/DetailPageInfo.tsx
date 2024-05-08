4383b096dea13f931b14c28cb20af1c5356580f3
Frontend <sweetysks23@gmail.com>
2024-05-08 10:29:20 +0900

    refactoring  : #165: 메인배너 수정, 디테일페이지 정보있을때만 표시

--- src/components/detail/DetailPageInfo.tsx
+++ src/components/detail/DetailPageInfo.tsx
@@ -9,22 +9,30 @@ const DetailPageInfo = () => {
   return (
     detailInfo && (
       <View style={styles.detailContainer}>
-        <View style={styles.detailItemList}>
-          <Text style={styles.itemTitle}>런타임</Text>
-          <Text style={styles.itemText}>{detailInfo?.prfruntime}</Text>
-        </View>
-        <View style={styles.detailItemList}>
-          <Text style={styles.itemTitle}>관람연령</Text>
-          <Text style={styles.itemText}>{detailInfo?.prfage}</Text>
-        </View>
-        <View style={styles.detailItemList}>
-          <Text style={styles.itemTitle}>출연진</Text>
-          <Text style={styles.itemText}>{detailInfo?.prfcast}</Text>
-        </View>
-        <View style={styles.detailItemList}>
-          <Text style={styles.itemTitle}>제작진</Text>
-          <Text style={styles.itemText}>{detailInfo?.prfcrew}</Text>
-        </View>
+        {detailInfo?.prfruntime && (
+          <View style={styles.detailItemList}>
+            <Text style={styles.itemTitle}>런타임</Text>
+            <Text style={styles.itemText}>{detailInfo?.prfruntime}</Text>
+          </View>
+        )}
+        {detailInfo?.prfage && (
+          <View style={styles.detailItemList}>
+            <Text style={styles.itemTitle}>관람연령</Text>
+            <Text style={styles.itemText}>{detailInfo?.prfage}</Text>
+          </View>
+        )}
+        {detailInfo?.prfcast && (
+          <View style={styles.detailItemList}>
+            <Text style={styles.itemTitle}>출연진</Text>
+            <Text style={styles.itemText}>{detailInfo?.prfcast}</Text>
+          </View>
+        )}
+        {detailInfo?.prfcrew && (
+          <View style={styles.detailItemList}>
+            <Text style={styles.itemTitle}>제작진</Text>
+            <Text style={styles.itemText}>{detailInfo?.prfcrew}</Text>
+          </View>
+        )}
       </View>
     )
   );
