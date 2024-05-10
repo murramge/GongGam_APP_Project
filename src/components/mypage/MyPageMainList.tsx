3a3ccf41fed46be30bc8ed4de0caee44c0143fa4
kangeunhwa <dmdgi37@naver.com>
2024-05-10 17:10:26 +0900

    refactoring:mymeeting

--- src/components/mypage/MyPageMainList.tsx
+++ src/components/mypage/MyPageMainList.tsx
@@ -1,10 +1,32 @@
+import useUserMettingsApi from '@pages/MyPage/hooks/useUserMettingsApi';
 import {colors} from '@styles/color';
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-
+import React, {useCallback} from 'react';
+import {StyleSheet, View, Text, FlatList, Image} from 'react-native';
+import Config from 'react-native-config';
 interface MyPageMainListProps {}
 
 const MyPageMainList = ({}: MyPageMainListProps) => {
+  const data = useUserMettingsApi();
+
+  const renderItem = useCallback(({item, index}) => {
+    console.log(item);
+    return (
+      <View style={{padding: 10}}>
+        <Image
+          style={{width: 60, height: 60, borderRadius: 100}}
+          source={{
+            uri: `${Config.KOPIS_IMAGE_BASE_URL}/${item.image_url}`,
+          }}
+        />
+        <Text
+          style={{width: 60, height: 60, fontSize: 10, padding: 5}}
+          numberOfLines={2}>
+          {item.perf_name}
+        </Text>
+      </View>
+    );
+  }, []);
+
   return (
     <View
       style={{
@@ -17,7 +39,12 @@ const MyPageMainList = ({}: MyPageMainListProps) => {
         borderColor: colors.GRAY_100,
       }}>
       <Text style={{fontSize: 15, color: colors.BLACK}}>내 모임</Text>
-      <View></View>
+      <View>
+        <FlatList
+          data={data.mymeetings}
+          renderItem={renderItem}
+          horizontal={true}></FlatList>
+      </View>
     </View>
   );
 };
