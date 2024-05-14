4da8def73b81ea2a61fba793d5efbca33ca71b76
kangeunhwa <dmdgi37@naver.com>
2024-05-15 00:25:35 +0900

    내모임 리스트 바로 보여지게 fix

--- src/pages/Community/CommunityDetail.tsx
+++ src/pages/Community/CommunityDetail.tsx
@@ -30,6 +30,7 @@ import Loading from '../../components/common/skeleton/Loading';
 import CommentsModal from '../../components/common/modals/CommentsModal';
 import Config from 'react-native-config';
 import Toast from 'react-native-toast-message';
+import useUserMettingsApi from '@pages/MyPage/hooks/useUserMettingsApi';
 
 const PosterImageWidth = 110;
 const PosterImageHeight = PosterImageWidth * 1.1;
@@ -90,6 +91,7 @@ const CommunityDetail = ({navigation, route}: CommunityDetailProps) => {
     if (!meeting) return;
     await joinMeeting(meeting?.id);
     await fetch();
+
     setIsJoinModalOpen(false);
   };
 
